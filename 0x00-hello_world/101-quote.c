33ca625b841a5d2ba2a66c393a52bfa8bc06ea49
Kizito24 <kizitochiazor@gmail.com>
2022-12-12 10:13:24 -0800

    22

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 
 /**
- *  * main - prints to the standard error
- *   * Return: Always 0 (Success)
- *    */
+ * main - prints to the standard error
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
