c9cd5a568be921a47182b353f182be65ade51c06
Kizito24 <kizitochiazor@gmail.com>
2022-12-12 10:15:28 -0800

    Task fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - prints to the screen using puts function
- *   * Return: If successful the exit status is zero
- *    */
+ * main - prints to the screen using puts function
+ * Return: If successful the exit status is zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
