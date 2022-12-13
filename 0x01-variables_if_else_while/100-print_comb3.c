a8fe4cc1076672ae863cc4d1c847e65b0f7ecad9
Kizito24 <kizitochiazor@gmail.com>
2022-12-12 20:26:15 -0800

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - This program prints all decimal
@@ -13,15 +13,14 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = i + 1; j <= 57; j++)
-			{
-				putchar(i);
-				putchar(j);
-				if ((j == 57) && (i == 56))
-					break;
-				putchar(44);
-				putchar(32);
-			}
-	}
-	putchar('\n');
-	return (0);
+		{
+			putchar(i);
+			putchar(j);
+			if ((j == 57) && (i == 56))
+			putchar(44);
+			putchar(32);
+		}
+						}
+					putchar('\n');
+						return (0);
 }
