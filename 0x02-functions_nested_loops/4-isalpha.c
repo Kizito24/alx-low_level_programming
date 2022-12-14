c35c20ff0513920071b1b673fae039627f573035
Kizito24 <kizitochiazor@gmail.com>
2022-12-14 14:51:59 -0800

    11

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - Checks for alphabetic characters
- * @c: This ia an ASCII character
+ * @c: This is an ASCII character
  *
  * Return: 0 if not an alphabet, 1 if so
  */
 int _isalpha(int c)
 {
 
-	if ((c >= 97) && (c <= 122) || (c >= 65) && (c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
