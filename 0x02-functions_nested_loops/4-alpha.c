f34749f1f6690969980ebda85557e78d9c62e730
Kizito24 <kizitochiazor@gmail.com>
2022-12-14 12:31:10 -0800

    4

---
+++ 0x02-functions_nested_loops/4-alpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic characters
+ * @c: This ia an ASCII character
+ *
+ * Return: 0 if not an alphabet, 1 if so
+ */
+int _isalpha(int c)
+{
+	int c;
+
+	if ((c >= 97) && (c <= 122) || (c >= 65) && (c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
