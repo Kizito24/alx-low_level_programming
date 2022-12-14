1a646da664b505e0dce4cf5c960d7fdbffe370f1
Kizito24 <kizitochiazor@gmail.com>
2022-12-14 12:13:19 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c - This is an ASCII character
+ * Return: 0 if not lowercase, 1 if lowercase
+ */
+int _islower(int c)
+{
+	int ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
