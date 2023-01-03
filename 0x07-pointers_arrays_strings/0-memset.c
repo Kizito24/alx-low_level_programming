38fb52146dd07ce9f7e05741d4f80d2267be7bf5
Kizito24 <kizitochiazor@gmail.com>
2023-01-03 03:53:46 -0800

    Task fix

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * _memset() - Function to fill out a buffer using a constant with a specified size
+ * @n - specified size in bytes
+ * @s - points out the buffer
+ * @b - char to be used to fill the buffer
+ *
+ * Return: the pointer to s memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int n;
+
+	for(i = 0; i < n; i++)
+	{
+		s[i];
+	}
+	return (s);
+}
