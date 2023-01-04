843b71b4c38d50d89408b578e833d59ca37abc64
Kizito24 <kizitochiazor@gmail.com>
2023-01-03 17:52:42 -0800

    task fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * -memcpy - Fills a buffer with a constant for a specified size
  * @src: This is the char that will be used to fill buffer
  * @dest: This is the buffer to be filled
