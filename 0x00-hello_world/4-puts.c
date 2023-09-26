1ab0849c1a5747a2370991d78d04459232fb5c73
Kizito24 <kizitochiazor@gmail.com>
2023-09-26 22:48:37 +0100

    Repushing projects

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the screen using puts function
+ * Return: If successful the exit status is zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
