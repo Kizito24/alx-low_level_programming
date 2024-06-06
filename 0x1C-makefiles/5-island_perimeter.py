a12818ede0854468fb9af6aa2daec1d6a9c1d1cf
Kizito <kizitochiazor@gmail.com>
2024-06-06 22:08:35 +0100

    Task fixes

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Created on 24th May, 2023
+
+Author:
+	Chiazor Kizito
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (List[List[int]]): The grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+
+    """
+
+    if not grid or not grid[0]:
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
